4e61339ae654e6104a19815d141ebf380a7c5c0e
Stephen Mwangi <st3v3n.mw@gmail.com>
2020-07-19 15:11:03 +0300

    changed file structure

---
+++ numbertheory/gcd.rs
@@ -0,0 +1,28 @@
+// Compute the GCD of m & n using Euclid's Algorithm
+fn gcd(m: i32, n: i32) -> i32 {
+    if n == 0 {
+        return m;
+    }
+    return gcd(n, m % n);
+}
+
+// Compute the GCD of a list
+fn gcd_arr(v: Vec<i32>) -> i32 {
+    let mut gcd_r = v[0];
+    for num in v {
+        gcd_r = gcd(gcd_r, num);
+    }
+    return gcd_r;
+}
+
+fn main() {
+    println!("{}", gcd(0, 0)); // 0
+    println!("{}", gcd(7, 0)); // 7
+    println!("{}", gcd(225, 144)); // 9
+    println!("{}", gcd(144, -225)); // 9
+    println!("{}", gcd(-144, 225)); // 9
+    println!("{}", gcd(-144, -225)); // 9
+
+    let v = vec![200, 500, 6000];
+    println!("{}", gcd_arr(v)); // 100
+}
