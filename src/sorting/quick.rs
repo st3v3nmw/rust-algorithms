7d9f282e4c3f3869eee9b8922575d23b67ec49d2
Francesco Micheli <mi.francesco@gmail.com>
2022-11-29 22:13:28 +0100

    Add Merge sort and Quick sort

--- src/sorting/quick.rs
+++ src/sorting/quick.rs
@@ -1,18 +1,44 @@
-// TODO
-
-fn quick_sort<T: Ord + Copy>(left: usize, right: usize, arr: &mut [T]) {
-    if right - left <= 0 {
-        return;
+fn quick_sort<T: Ord>(arr: &mut [T], low: isize, high: isize) {
+    if low < high {
+        let p = partition(arr, low, high);
+        quick_sort(arr, low, p-1);
+        quick_sort(arr, p + 1, high);
     }
-
-    let pivot: T = arr[left];
-    let pivot_idx: usize = right / 2;
-    quick_sort(left, (pivot_idx as isize - 1) as usize, arr);
-    quick_sort(pivot_idx + 1, right, arr)
 }
 
-fn main() {
-    let mut arr: Vec<i32> = vec![3, -2, 9, 0, 12, -5, 8, 0];
-    quick_sort(0, arr.len(), &mut arr);
-    assert_eq!(arr, vec![-5, -2, 0, 0, 3, 8, 9, 12]);
+fn partition<T:Ord>(arr: &mut [T], low: isize, high: isize) -> isize {
+    let pivot = high as usize;
+    let mut store_index = low - 1;
+    let mut last_index = high;
+    loop {
+        store_index += 1;
+        while arr[store_index as usize] < arr[pivot] {
+            store_index += 1;
+        }
+        last_index -= 1;
+        while last_index >= 0 && arr[last_index as usize] > arr[pivot] {
+            last_index -= 1;
+        }
+        if store_index >= last_index {
+            break;
+        } else {
+            arr.swap(store_index as usize, last_index as usize);
+        }
+    }
+    arr.swap(store_index as usize, pivot as usize);
+    store_index
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn quick_sort_test() {
+        let mut arr: Vec<i32> = vec![3, -2, 9, 0, 12, -5, 8, 0];
+        let len = arr.len();
+        quick_sort(&mut arr, 0, (len - 1) as isize);
+        assert_eq!(arr, vec![-5, -2, 0, 0, 3, 8, 9, 12]);
+    }
 }
