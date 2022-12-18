b2c72e115c583e806d7771ca040b2e8edae6a521
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2022-12-18 23:32:53 +0300

    add sudoku

--- src/backtracking/sudoku.rs
+++ src/backtracking/sudoku.rs
@@ -1,3 +1,4 @@
+
 pub fn is_in_row(board: &[[i8;9];9], row: &usize, number: &i8) -> bool{
     for col in 0..9 {
         if board[*row][col] == *number {
@@ -21,15 +22,21 @@ pub fn is_in_box(board: &[[i8;9];9], row: &usize, col: &usize, number: &i8) -> b
     let l_row = *&row - (*&row % 3);
     for row in l_row..l_row + 3 {
         for col in l_col..l_col + 3 {
-            if board[row][col] == *number {return true;}
+            if board[row][col] == *number {
+                return true;
+            }
         }
     }
     return false;
 }
 
 pub fn is_valid_placement(board: &[[i8;9];9], row: &usize, col: &usize, number: &i8) -> bool {
-    if !is_in_box(board, row, col, number) && !is_in_column(board, col, number) && !is_in_row(board, row, number) {return true;}
-    else {return false;}
+    if !is_in_box(board, row, col, number) && !is_in_column(board, col, number) && !is_in_row(board, row, number) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 pub fn solve(board: &mut [[i8;9];9]) -> bool {
@@ -44,7 +51,9 @@ pub fn solve(board: &mut [[i8;9];9]) -> bool {
                            board[*&i][*&j] = 0;
                            empties += 1;
                        }
-                       else {return true;}
+                       else {
+                        return true;
+                    }
                     }
                 }
                 return false;
@@ -60,7 +69,7 @@ mod tests {
 
     #[test]
     fn solve_board_test() {
-        let mut board = [[7,5,2,0,5,0,6,0,0],
+        let mut board1 = [[7,5,2,0,5,0,6,0,0],
                         [0,0,0,0,0,3,0,0,0],
                         [1,0,0,0,0,9,5,0,0],
                         [8,0,0,0,0,0,0,9,0],
@@ -70,6 +79,16 @@ mod tests {
                         [0,0,0,2,0,0,0,0,0],
                         [0,0,7,0,4,0,2,0,3]];
 
-        assert_eq!(solve(&mut board), false);
+        let mut board2 = [[7,0,2,0,5,0,6,0,0],
+                        [0,0,0,0,0,3,0,0,0],
+                        [1,0,0,0,0,9,5,0,0],
+                        [8,0,0,0,0,0,0,9,0],
+                        [0,4,3,0,0,0,7,5,0],
+                        [0,9,0,0,0,0,0,0,8],
+                        [0,0,9,7,0,0,0,0,5],
+                        [0,0,0,2,0,0,0,0,0],
+                        [0,0,7,0,4,0,2,0,3]];
+        assert_eq!(solve(&mut board1), false);
+        assert_eq!(solve(&mut board2), true);
     }
 }
